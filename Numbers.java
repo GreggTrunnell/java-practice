569f67afd25267927015c8397109889367ffdc29
Gregg Trunnell <gggtrunnell@gmail.com>
2025-09-30 14:32:51 -0500

    pushing everything

--- Numbers.java
+++ Numbers.java
@@ -1,2 +1,8 @@
 public class Numbers {
-}
+    public static void main(String[] args){
+    double numDouble = 12.40;
+System.out.println("Double value: " + numDouble);
+    // convert a double to an int rounded to nearest value
+    int numInt = (int) (numDouble + 0.5);
+System.out.println("Int value: " + numInt);
+}}
